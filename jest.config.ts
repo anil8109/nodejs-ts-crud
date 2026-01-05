8cff4725d1be0d100955348607d3fdb83e1f675f
anil8109 <anil810966@gmail.com>
2026-01-06 00:41:45 +0530

    chore: initial setup with ts, jest, ci, gitignore

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/server.ts'
+  ],
+
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
+
+export default config;
